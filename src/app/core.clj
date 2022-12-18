db0e4683ebae6d8d34bd833308440ffeaf8cd5c5
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-12-19 00:13:02 +0400

    update wt

--- src/app/core.clj
+++ src/app/core.clj
@@ -40,83 +40,81 @@
 
 (def layout
   (wt/compile
-   [<>
-    "<!doctype html>"
-    [html {lang en}
-     [head
-      [meta {charset "utf-8"}]
-      [meta {name          "viewport"
-             content       "width=device-width"
-             initial-scale "1"}]
-      [title "Bootstrap demo"]
-      [link {href        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-             rel         "stylesheet"
-             integrity   "sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-             crossorigin "anonymous"}]]
-     [body
-      [nav.navbar.navbar-expand-lg.bg-light
-       [.container
-        [a.navbar-brand {href "#"}
-         "Twitter clone clj"]
-        [button.navbar-toggler {type           "button"
-                                data-bs-toggle "collapse"
-                                data-bs-target "#navbarSupportedContent"
-                                aria-controls  "navbarSupportedContent"
-                                aria-expanded  "false"
-                                aria-label     "Toggle navigation"}
-         [span.navbar-toggler-icon]]
-        [.collapse.navbar-collapse#navbarSupportedContent
-         [ul.navbar-nav.me-auto.mb-2.mb-lg-0
-          [li.nav-item
-           [a.nav-link.active {aria-current "page"
-                               href         "#"}
-            "Home"]]]]]]
+   '[<>
+     "<!doctype html>"
+     [html {lang en}
+      [head
+       [meta {charset "utf-8"}]
+       [meta {name          "viewport"
+              content       "width=device-width"
+              initial-scale "1"}]
+       [title "Bootstrap demo"]
+       [link {href        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+              rel         "stylesheet"
+              integrity   "sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+              crossorigin "anonymous"}]]
+      [body
+       [nav.navbar.navbar-expand-lg.bg-light
+        [.container
+         [a.navbar-brand {href "#"}
+          "Twitter clone clj"]
+         [button.navbar-toggler {type           "button"
+                                 data-bs-toggle "collapse"
+                                 data-bs-target "#navbarSupportedContent"
+                                 aria-controls  "navbarSupportedContent"
+                                 aria-expanded  "false"
+                                 aria-label     "Toggle navigation"}
+          [span.navbar-toggler-icon]]
+         [.collapse.navbar-collapse#navbarSupportedContent
+          [ul.navbar-nav.me-auto.mb-2.mb-lg-0
+           [li.nav-item
+            [a.nav-link.active {aria-current "page"
+                                href         "#"}
+             "Home"]]]]]]
 
-      (:body)
+       (:body)
 
-      [script {src         "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-               integrity   "sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-               crossorigin "anonymous"}]
-      [script {src         "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-               integrity   "sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-               crossorigin "anonymous"}]]]]))
+       [script {src         "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+                integrity   "sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+                crossorigin "anonymous"}]
+       [script {src         "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+                integrity   "sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+                crossorigin "anonymous"}]]]]))
 
-(def template
+(def body-tmpl
   (wt/compile
-   [.container
-    (:new-tweet)
-    (:tweets)]))
+   '[.container
+     (:new-tweet (:tmpl))
+     (:tweets (:tmpl))]))
 
-(defn bind [renderable ctx]
-  (reify wtp/Renderable
-    (render [_ w _]
-      (wtp/render renderable w ctx))))
-
-(def tweet
+(def tweet-tmpl
   (wt/compile
-   [.card.my-4
-    [.card-body
-     (:text)]
-    [.card-footer
-     #_(:created-at)
-     [a {href (:author-url)}
-      (:author)]
-     #_(:likes)]]))
+   '[.card.my-4
+     [.card-body
+      (:text)]
+     [.card-footer
+      #_(:created-at)
+      [a {href (:author-url)}
+       (:author)]
+      #_(:likes)]]))
 
 (defn tweet-presenter [tweet]
   (-> tweet
+      (assoc :tmpl #'tweet-tmpl)
       (assoc :author-url "#")))
 
-
-(def new-tweet
+(def new-tweet-tmpl
   (wt/compile
-   [.card.my-4
-    [.card-body
-     [form
-      [.mb-3
-       [textarea.form-control {rows 3}]]
-      [button.btn.btn-primary {type submit}
-       "Tweet"]]]]))
+   '[.card.my-4
+     [.card-body
+      [form
+       [.mb-3
+        [textarea.form-control {rows 3}]]
+       [button.btn.btn-primary {type submit}
+        "Tweet"]]]]))
+
+(defn new-tweet-presenter []
+  {:tmpl #'new-tweet-tmpl})
 
 (defn handler [-deps req]
   (let [tweets [{:text   "awesome"
@@ -124,9 +122,9 @@
                 {:text   "Hi"
                  :author "@jane"}]]
 
-    (-> (wt/render-to-string #'layout {:body      #'template
-                                       :new-tweet #'new-tweet
-                                       :tweets    (->> tweets
-                                                       (map tweet-presenter)
-                                                       (map (partial bind #'tweet)))})
+    (-> (wt/render-to-string #'layout {:body      #'body-tmpl
+                                       :new-tweet (new-tweet-presenter)
+                                       :tweets    (map tweet-presenter tweets)})
+
+
         (ring.resp/ok))))
