9f4a0464b2bd12fdcd52f2bff856ac4ae50560b5
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2023-01-02 19:39:55 +0400

    save

--- src/app/web/crud.clj
+++ src/app/web/crud.clj
@@ -5,30 +5,30 @@
    [darkleaf.web-template.core :as wt]
    [darkleaf.web-template.ring :as wt.ring]
    [reitit.core :as r]
-   [ring.util.http-response :as ring.resp]))
+   [ring.util.http-response :as ring.resp]
+   [clojure.string :as str]))
 
-#_
-{:login            "john"
- [:errors :login]  [{...}]
- :nested           [...]
- [:errors :nested] [{...}]}
+(defn- path [req name]
+  (-> req
+      ::r/router
+      (r/match-by-name name)
+      (r/match->path)))
 
+(defn- ->str [x]
+  (cond
+    (keyword? x) (-> x symbol str)
+    :else (str x)))
 
-;; fail fast
-(defn text [#_{:required true, min max}]
-  (fn [x]
-    x))
+(defn- path->input-name [path]
+  (let [[head & tail] path]
+    (str
+     (->str head)
+     (->> tail
+          (map #(str "[" (->str %) "]"))
+          (str/join)))))
 
-(defn coerce [params schema]
-  (persistent!
-   (reduce-kv (fn [acc key coercer]
-                (let [key-name (name key)
-                      value    (get params key-name)
-                      ;;todo: coercer as map
-                      value    (coercer value)]
-                  (assoc! acc key value)))
-              (transient {})
-              schema)))
+(defn- path->input-id [path]
+  (->> path (map ->str) (str/join "_")))
 
 (def index-tmpl
   (wt/compile
@@ -36,19 +36,16 @@
      [a.btn.btn-primary {href (:new-entity-url)}
       "New entity"]]))
 
-(defn index-presenter [{::r/keys [router]}]
+(defn index-presenter [req]
   {::wt/renderable #'layout/layout-tmpl
    :body           {::wt/renderable #'index-tmpl
-                    :new-entity-url (-> router
-                                        (r/match-by-name ::new)
-                                        (r/match->path))}})
+                    :new-entity-url (path req :crud/new)}})
 
 (defn index-action [-deps req]
   (-> (wt.ring/body (index-presenter req))
       (ring.resp/ok)
       (ring.resp/content-type "text/html; charset=utf-8")))
 
-(defn show-action [-deps req])
 
 (def input-tmpl
   (wt/compile
@@ -57,6 +54,15 @@
       (:label)]
      [input.form-control {id  (:id)
                           ... (:input-attrs)}]]))
+
+(defn input-presenter [path]
+  (let [id   (path->input-id path)
+        name (path->input-name path)]
+    {::wt/renderable #'input-tmpl
+     :id             id
+     :label          (last path)
+     :input-attrs    {:name name}}))
+
 
 (def new-tmpl
   (wt/compile
@@ -67,26 +73,24 @@
                              data-action     "form-validator#validate"}
       (:string)
       (:required-string)
+      (:number)
       [button.btn.btn-primary {type submit}
        "Submit"]]]))
 
-(defn new-presenter [{::r/keys [router]}]
+
+
+(defn new-presenter [req]
   {::wt/renderable #'layout/layout-tmpl
    :body           {::wt/renderable  #'new-tmpl
-                    :action          (-> router
-                                         (r/match-by-name ::index)
-                                         (r/match->path))
-                    :string          {::wt/renderable #'input-tmpl
-                                      :id             "string"
-                                      :label          "String"
-                                      :input-attrs    {:type "text"
-                                                       :name "form[string]"}}
-                    :required-string {::wt/renderable #'input-tmpl
-                                      :id             "required-string"
-                                      :label          "Required string"
-                                      :input-attrs    {:type     "text"
-                                                       :required true
-                                                       :name     "form[required-string]"}}}})
+                    :action          (path req :crud/new)
+                    :string          (-> (input-presenter [:form :string])
+                                         (update :input-attrs merge {:type "string"}))
+                    :required-string (-> (input-presenter [:form :required-string])
+                                         (update :input-attrs merge {:type     "string"
+                                                                     :required true}))
+                    :number          (-> (input-presenter [:form :number])
+                                         (update :input-attrs merge {:type "text"
+                                                                     :pattern #"\d+"}))}})
 
 (defn new-action [-deps req]
   (-> (wt.ring/body (new-presenter req))
@@ -96,15 +100,14 @@
 (defn create-action [-deps req]
   (let [form (-> req
                  :params
-                 (get "form")
-                 (coerce {:string          (text)
-                          :required-string (text)}))]
+                 (get "form"))]
     (prn form)))
 
 (def route-data
   (di/template
-   [["" {:name ::index
-         :get  (di/ref `index-action)
-         :post (di/ref `create-action)}]
-    ["/new" {:name ::new
-             :get  (di/ref `new-action)}]]))
+   [["" {:name :crud/index
+         :get  (di/ref `index-action)}]
+
+    ["/new" {:name :crud/new
+             :get  (di/ref `new-action)
+             :post (di/ref `create-action)}]]))
