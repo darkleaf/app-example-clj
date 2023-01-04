16781392920cd2fc357da94a724ab0d6bc842b56
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2023-01-04 14:11:12 +0400

    plain old froms

--- src/app/web/crud.clj
+++ src/app/web/crud.clj
@@ -14,22 +14,6 @@
       (r/match-by-name name)
       (r/match->path)))
 
-(defn- ->str [x]
-  (cond
-    (keyword? x) (-> x symbol str)
-    :else (str x)))
-
-(defn- path->input-name [path]
-  (let [[head & tail] path]
-    (str
-     (->str head)
-     (->> tail
-          (map #(str "[" (->str %) "]"))
-          (str/join)))))
-
-(defn- path->input-id [path]
-  (->> path (map ->str) (str/join "_")))
-
 (def index-tmpl
   (wt/compile
    '[<>
@@ -46,47 +30,26 @@
       (ring.resp/ok)
       (ring.resp/content-type "text/html; charset=utf-8")))
 
-
-(def input-tmpl
-  (wt/compile
-   '[.mb-3 {data-controller        "input"
-            data-input-t-key-value (:t-key)}
-     [label.form-label {for               (:id)
-                        data-input-target "label"}
-      (:label)]
-     [input.form-control {id                (:id)
-                          data-input-target "input"
-                          ...               (:input-attrs)}]]))
-
-(defn input-presenter [form field]
-  (let [id         (str/join "_" [(name form) (name field)])
-        t-key      (str/join "." ["forms" (name form) (name field)])
-        input-name (str (name form) "[" (name field) "]")]
-    {::wt/renderable #'input-tmpl
-     :t-key          t-key
-     :id             id
-     :input-attrs    {:name input-name}}))
-
 (def new-tmpl
   (wt/compile
    '[form.needs-validation {action          (:action)
                             method          post
                             data-controller "form-validator"
                             data-action     "form-validator#validate"}
-     (:string)
-     (:required-string)
-     (:number)
+     [.mb-3 {data-controller input data-input-t-key-value "string"}
+      [label.form-label {for               crud_string
+                         data-input-target label}]
+      [input.form-control {id                crud_string
+                           name              "crud[string]"
+                           data-input-target input}]]
      [button.btn.btn-primary {type submit}
       "Submit"]]))
 
 
 (defn new-presenter [req]
   {::wt/renderable #'layout/layout-tmpl
-   :body           {::wt/renderable  #'new-tmpl
-                    :action          (path req :crud/new)
-                    :string          (input-presenter :crud :string)
-                    :required-string (input-presenter :crud :required-string)
-                    :number          (input-presenter :crud :number)}})
+   :body           {::wt/renderable #'new-tmpl
+                    :action         (path req :crud/new)}})
 
 (defn new-action [-deps req]
   (-> (wt.ring/body (new-presenter req))
@@ -96,7 +59,7 @@
 (defn create-action [-deps req]
   (let [form (-> req
                  :params
-                 (get "form"))]
+                 (get "crud"))]
     (prn form)))
 
 (def route-data
